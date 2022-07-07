4ec40d38a7054a8ddb237c250d997bfd9cf16558
AAKAAASSHHH24 <akashrksht@gmail.com>
2022-07-07 17:54:13 +0530

    1st try

--- housing/component/data_ingestion.py
+++ housing/component/data_ingestion.py
@@ -13,7 +13,7 @@ class DataIngestion:
 
     def __init__(self,data_ingestion_config:DataIngestionConfig ):
         try:
-            logging.info(f"{'='*20}Data Ingestion log started.{'='*20} ")
+            logging.info(f"{'>>'*20}Data Ingestion log started.{'<<'*20} ")
             self.data_ingestion_config = data_ingestion_config
 
         except Exception as e:
@@ -28,9 +28,6 @@ class DataIngestion:
             #folder location to download file
             tgz_download_dir = self.data_ingestion_config.tgz_download_dir
             
-            if os.path.exists(tgz_download_dir):
-                os.remove(tgz_download_dir)
-
             os.makedirs(tgz_download_dir,exist_ok=True)
 
             housing_file_name = os.path.basename(download_url)
@@ -130,4 +127,4 @@ class DataIngestion:
 
 
     def __del__(self):
-        logging.info(f"{'='*20}Data Ingestion log completed.{'='*20} \n\n")
+        logging.info(f"{'>>'*20}Data Ingestion log completed.{'<<'*20} \n\n")
